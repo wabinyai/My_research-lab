7749f8152c771e9c448bcf7f7da3ea31be968c7d
wabinyai <rajacastro@gmail.com>
2023-11-07 15:21:46 +0300

    getis ord

--- src/Getis-Ord-Local-Statistic/main.py
+++ src/Getis-Ord-Local-Statistic/main.py
@@ -3,18 +3,13 @@ from datetime import datetime
 from datetime import timedelta
 
 # Specify the airqloud_id
-airqloud_id = "641b3069572090002992a7a1"
+airqloud_id = "61815b91e2dcb4002aad0777"
 
 # Usage example:
 start_time = datetime(2023, 10, 1, 9, 0, 0)
 end_time = datetime(2023, 11, 6, 9, 0, 0)
 
-# Call the function with the desired start and end times
 data = fetch_data_from_api(airqloud_id, start_time, end_time)
-
-
-# Fetch data from the API
-#data = fetch_data_from_api(airqloud_id)
 
 if data:
     # Get the GeoDataFrame with relevant data
@@ -24,8 +19,6 @@ if data:
     # Calculate Local Moran's I
     g_local, significant_hot_spots_99, significant_hot_spots_95, significant_hot_spots_90, significant_cold_spots_99, significant_cold_spots_95, significant_cold_spots_90 = Getis_Ord_Local_regression(gdf)
 
-
-    # Plot the results
     plot_Getis_Ord_local(g_local,  significant_hot_spots_99, significant_hot_spots_95, significant_hot_spots_90, significant_cold_spots_99, significant_cold_spots_95, significant_cold_spots_90, gdf)
 else:
     print("Failed to fetch data from the API.")
