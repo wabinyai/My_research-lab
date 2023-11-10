b6793a72d7d2ba57dabae22e7a31789e6c015095
wabinyai <rajacastro@gmail.com>
2023-11-10 17:42:52 +0300

    how to deal with null in local

--- src/local_moran_with_API/main.py
+++ src/local_moran_with_API/main.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from datetime import timedelta
 
 # Specify the airqloud_id
-airqloud_id = "61815b91e2dcb4002aad0777"
+airqloud_id = "618b82069326560036a45344"
 
 # Usage example:
 start_time = datetime(2023, 10, 5, 9, 0, 0)
@@ -14,14 +14,18 @@ data = fetch_data_from_api(airqloud_id, start_time, end_time)
 
 if data:
     # Get the GeoDataFrame with relevant data
-    gdf = get_data_for_moran(data)
-    gdf= gdf.dropna(subset='calibratedValue')
-    print(gdf.info())
+    gdf = get_data_for_moran(data) 
+    # Check if 'calibratedValue' is present before dropping NaN values
+    if 'calibratedValue' in gdf.columns:
+        print("Number of NaN values in calibratedValue:", gdf['calibratedValue'].isna().sum())
+        gdf= gdf.dropna(subset='calibratedValue')
+        print(gdf.info())
 
     # Calculate Local Moran's I
-    moran_loc = moran_local_regression(gdf)
+        moran_loc = moran_local_regression(gdf)
+        plot_moran_local(moran_loc, gdf)
 
-    # Plot the results
-    plot_moran_local(moran_loc, gdf)
+    else:
+         print("No measurements for this search.")
 else:
     print("Failed to fetch data from the API.")
