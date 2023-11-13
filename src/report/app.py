eb387dfdbb56cfd014b6cb15ad00191e35722125
wabinyai <rajacastro@gmail.com>
2023-11-13 23:02:55 +0300

    Report

--- src/report/app.py
+++ src/report/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from configure import Config
+from datetime import datetime
 from utils import fetch_air_quality_data, generate_report
 
 app = Flask(__name__)
@@ -9,13 +10,13 @@ app.config.from_object(Config)
 def index():
     if request.method == 'POST':
         grid_id = request.form['grid_id']
-        start_time = request.form['start_time']
-        end_time = request.form['end_time'] 
+        start_time = datetime.strptime(request.form.get('start_time'), '%Y-%m-%dT%H:%M')
+        end_time = datetime.strptime(request.form.get('end_time'), '%Y-%m-%dT%H:%M')
         page = 1
 
         air_quality_data = fetch_air_quality_data(grid_id, start_time, end_time,page)
         report_data = generate_report(air_quality_data)
-
+    
         return render_template('report.html', report_data=report_data)
     return render_template('index.html')
 
