4c1b5c1992a2a4644f3c5cc40411ec7b1eef0604
wabinyai <rajacastro@gmail.com>
2023-11-24 13:36:14 +0300

    delete

--- src/map_predicted_pm/configure.py
+++
@@ -1,4 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key')
--- src/map_predicted_pm/utils.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-def get_pm25_data(latitude, longitude):
-    api_url = f'https://platform.airqo.net/api/v2/predict/search?latitude={latitude}&longitude={longitude}&token=your_api_token'
-    response = requests.get(api_url)
-    data = response.json()
-    return data
