0d56a1dffe3c977c9dfc35fc923180b243bae8ea
wabinyai <rajacastro@gmail.com>
2024-04-15 16:01:51 +0300

    insert into bigquery

---
+++ src/Inserting_value_in_bigQuery/bigquery_save.py
@@ -0,0 +1,67 @@
+import os
+from datetime import datetime
+from tqdm import tqdm  # Import tqdm library for progress bar
+
+import pandas as pd
+from google.cloud import bigquery
+from google.oauth2 import service_account
+from config import Config
+
+def column_mapping(file):
+    data = pd.read_csv(file)
+    renaming = {'device_latitude': 'latitude',
+                'device_longitude': 'longitude',
+                'device_name': 'device_id',
+                's1_pm2_5': 's1_pm2_5',
+                'pm2_5': 'pm2_5',
+                # 's2_pm2_5': 's2_pm2_5',
+                'datetime': 'timestamp',
+                'tenant': 'tenant',
+               # 'site_id': 'site_id',
+                # 'site_name': ,
+                # 'site_latitude': 'latitude',
+                # 'site_longitude': 'longitude',
+                # 'device_name_1': '',
+                # 'frequency': 
+                }
+    data.rename(columns = renaming, inplace=True)
+    data['s1_pm2_5'] = pd.to_numeric(data['s1_pm2_5'], errors='coerce')
+    
+    # Parse and reformat 'timestamp' column
+    data['timestamp'] = pd.to_datetime(data['timestamp'], errors='coerce')
+    data['timestamp'] = data['timestamp'].dt.strftime('%Y-%m-%d %H:%M:%S')
+    data_sub = data[['tenant', 'timestamp', 'site_id', 'device_id', 'latitude', 'longitude', 'pm2_5']]
+    data_sub.fillna('', inplace=True)
+
+    return data_sub
+
+def save_predictions_on_bigquery(file):
+    data = column_mapping(file)
+    data_json = data.to_dict(orient='records')
+
+    client = bigquery.Client()
+
+    # Calculate the total number of rows for the progress bar
+    total_rows = len(data_json)
+    
+    with tqdm(total=total_rows, desc="Inserting rows to BigQuery....") as pbar:
+        # Iterate through data_json and insert rows
+        for chunk in chunks(data_json, chunk_size=1000):  # Adjust chunk size as per your requirement
+            errors = client.insert_rows_json( 
+             #   json_rows=chunk, table=Config.BIGQUERY_DEVICE_MEASUREMENT_STAGE, skip_invalid_rows=True
+                json_rows=chunk, table=Config.BIGQUERY_DEVICE_MEASUREMENT_PROD, skip_invalid_rows=True
+            )
+            if errors:
+                print("Encountered errors while inserting rows:", errors)
+            else:
+                pbar.update(len(chunk))  # Update progress bar with the number of rows inserted
+        print("Data inserted successfully!")
+
+def chunks(lst, chunk_size):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), chunk_size):
+        yield lst[i:i + chunk_size]
+
+if __name__ == "__main__":
+    file = 'D:/AIRQO/data/ArbaMinch_AirQo_files/sp12.csv'
+    save_predictions_on_bigquery(file=file)
---
+++ src/Inserting_value_in_bigQuery/config.py
@@ -0,0 +1,30 @@
+import os
+
+import urllib3
+from pymongo import MongoClient
+from dotenv import load_dotenv
+from pathlib import Path
+BASE_DIR = Path(__file__).resolve().parent
+dotenv_path = os.path.join(BASE_DIR, '.env')
+load_dotenv(dotenv_path)
+
+
+
+class Config:
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    CREDENTIALS = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
+    MEASUREMENTS_TOPIC = os.getenv('MEASUREMENTS_TOPIC')
+    AIRQLOUDS_TOPIC = os.getenv('AIRQLOUDS_TOPIC')
+    BOOTSTRAP_SERVERS = os.getenv('BOOTSTRAP_SERVERS')
+    GP_MODEL_DB = os.getenv('GP_MODEL_DB')
+    GP_MODEL_DB_URI = os.getenv('GP_MODEL_DB_URI')
+
+    BIGQUERY_MEASUREMENTS_PREDICTIONS = os.getenv('BIGQUERY_MEASUREMENTS_PREDICTIONS')
+    BIGQUERY_SITES = os.getenv("BIGQUERY_SITES")
+    BIGQUERY_AIRQLOUDS_SITES = os.getenv("BIGQUERY_AIRQLOUDS_SITES")
+    BIGQUERY_HOURLY_DATA = os.getenv("BIGQUERY_HOURLY_DATA")
+    BIGQUERY_DEVICE_MEASUREMENT_STAGE = os.getenv("BIGQUERY_DEVICE_MEASUREMENT_STAGE")
+    BIGQUERY_DEVICE_MEASUREMENT_PROD = os.getenv("BIGQUERY_DEVICE_MEASUREMENT_PROD")
